511033ce8656d02dbaf02ece6c4cb573fa19248f
kachamozo <cachamocito@yahoo.com>
2024-12-10 23:18:53 -0400

    ejercicios de string

--- Basic/06-strings.js
+++ Basic/06-strings.js
@@ -7,32 +7,32 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6565
 
 // Concatenación
 
-let myName = "Brais"
-let greeting = "Hola, " + myName + "!"
-console.log(greeting)
-console.log(typeof greeting)
+let myName = 'Brais';
+let greeting = 'Hola, ' + myName + '!';
+console.log(greeting);
+console.log(typeof greeting);
 
 // Longitud
 
-console.log(greeting.length)
+console.log(greeting.length);
 
 // Acceso a caracteres
 
-console.log(greeting[0])
-console.log(greeting[11])
+console.log(greeting[0]);
+console.log(greeting[11]);
 
 // Métodos comunes
 
-console.log(greeting.toUpperCase()) // Mayúsculas
-console.log(greeting.toLowerCase()) // Minúsculas
-console.log(greeting.indexOf("Hola")) // Índice
-console.log(greeting.indexOf("Brais"))
-console.log(greeting.indexOf("MoureDev"))
-console.log(greeting.includes("Hola")) // Incluye
-console.log(greeting.includes("Brais"))
-console.log(greeting.includes("MoureDev"))
-console.log(greeting.slice(0, 10)) // Sección
-console.log(greeting.replace("Brais", "MoureDev")) // Reemplazo
+console.log(greeting.toUpperCase()); // Mayúsculas
+console.log(greeting.toLowerCase()); // Minúsculas
+console.log(greeting.indexOf('Hola')); // Índice
+console.log(greeting.indexOf('Brais'));
+console.log(greeting.indexOf('MoureDev')); // Si no existe marca -1
+console.log(greeting.includes('Hola')); // Incluye
+console.log(greeting.includes('Brais'));
+console.log(greeting.includes('MoureDev')); // Si no incluye marca false
+console.log(greeting.slice(0, 10)); // Sección
+console.log(greeting.replace('Brais', 'MoureDev')); // Reemplazo
 
 // Template literals (plantillas literales)
 
@@ -40,9 +40,9 @@ console.log(greeting.replace("Brais", "MoureDev")) // Reemplazo
 let message = `Hola, este
 es mi
 curso de
-JavaScript`
-console.log(message)
+JavaScript`;
+console.log(message);
 
 // Interpolación de valores
-let email = "braismoure@mouredev.com"
-console.log(`Hola, ${myName}! Tu email es ${email}.`)
+let email = 'braismoure@mouredev.com';
+console.log(`Hola, ${myName}! Tu email es ${email}.`);
--- Basic/07-strings-exercises.js
+++ Basic/07-strings-exercises.js
@@ -5,20 +5,51 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=7226
 
 // 1. Concatena dos cadenas de texto
 
+let nombre = 'Roly Gonzalo';
+let saludo = 'Hola';
+
+console.log(saludo + ' ' + nombre);
+
 // 2. Muestra la longitud de una cadena de texto
+
+console.log(nombre.length);
 
 // 3. Muestra el primer y último carácter de un string
 
+console.log(nombre[0]);
+console.log(nombre[nombre.length - 1]);
+
 // 4. Convierte a mayúsculas y minúsculas un string
+
+console.log(nombre.toLocaleUpperCase());
+console.log(nombre.toLocaleLowerCase());
 
 // 5. Crea una cadena de texto en varias líneas
 
+let cadena = `Esta es una cadena
+de texto
+en varias líneas
+`;
+console.log(cadena);
+
 // 6. Interpola el valor de una variable en un string
+
+console.log(`Mi nombre es ${nombre}`);
 
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
 
+console.log(cadena.replaceAll(' ', '-'));
+
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+console.log(cadena.includes('texto'));
 
 // 9. Comprueba si dos strings son iguales
 
+let a = 'Carlos';
+let b = 'Carlos';
+console.log(a == b);
+
 // 10. Comprueba si dos strings tienen la misma longitud
+
+console.log(cadena.length === nombre.length);
